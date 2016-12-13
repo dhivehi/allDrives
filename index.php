1f5e24ebbbae9fd381236f9e64e979c32764aa6d
DhivehiMv <admin@dhivehi.mv>
2016-12-14 00:05:09 +0500

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,6 @@ include('./gDrive.php');
 
 $gdrive    = new Dhivehi\gDrive();
 
-//$gdrive->file_put_contents("/path/to/file.db", "FOLDER_ID"); //less than 5mb
-//$gdrive->largeUpload("/path/to/file.db", "FOLDER_ID"); //greater than 5 mb
+//$gdrive->copy("/path/to/file.db", "/new/path/to/file.db"); //source path, dest path
 //$gdrive->scandir("FOLDER_ID"); //returns directory listing
 //$gdrive->mkdir("/www/my/test"); //recursive directory creation - returns FOLDER_ID on success
