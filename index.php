17c18a197779144eb82529f22855cc9d65d1b27a
DhivehiMv <admin@dhivehi.mv>
2017-04-09 13:36:31 +0500

    Update index.php

--- index.php
+++ index.php
@@ -5,5 +5,5 @@ include('./src/gDrive.php');
 $gdrive    = new Dhivehi\gDrive();
 
 //$gdrive->copy("/path/to/file.db", "/new/path/to/file.db"); //source path, dest path
-//$gdrive->scandir("FOLDER_ID"); //returns directory listing
+//$gdrive->scandir("/"); //returns directory listing
 //$gdrive->mkdir("/www/my/test"); //recursive directory creation - returns FOLDER_ID on success
